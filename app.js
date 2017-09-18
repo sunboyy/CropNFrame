c07d95ad099f0abf7581c0a7b72110c33a0961c8
sunboyy <sura_sun@hotmail.com>
2017-09-18 19:18:22 +0700

    Make design structure of a page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('node_modules/bootstrap/dist'));
 app.use('/js', express.static('node_modules/jquery/dist'));
 app.use('/js', express.static('node_modules/cropper/dist'));
+app.use(express.static('public'));
 
 require('./router')(app);
 
