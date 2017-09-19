fbcf9f8dfa286e2f391c756cdb364465bf2aae4c
sunboyy <sura_sun@hotmail.com>
2017-09-19 20:11:11 +0700

    Trying to read file

--- public/js/cropnframe.js
+++ public/js/cropnframe.js
@@ -12,4 +12,9 @@ $('#fileField').change(function() {
     // var imageb64 = $(this).val();
     // imageb64=imageb64.substr(imageb64.indexOf(',')+1,imageb64.length);
     $('#showSrcField').val(this.files[0].name);
+    var fileReader = new FileReader();
+    fileReader.onload = function(e) {
+        console.log(e.target.result);
+    }
+    fileReader.readAsDataURL(this.files[0].fillPath);
 });
