6971e90aafc88bb30ae3e2f8efb802678adf8258
sunboyy <sura_sun@hotmail.com>
2017-09-19 18:29:11 +0700

    Function "Next" and "Start Over" button

--- public/js/cropnframe.js
+++ public/js/cropnframe.js
@@ -1,17 +1,15 @@
 var app = angular.module("cropnframe", []);
 app.controller('cropnframe', function($scope) {
     $scope.step = 1;
-    $scope.file = {name: ""};
-    $scope.filename = function() {
-        return $scope.file.name;
+    $scope.next = function() {
+        $scope.step++;
     }
-    $scope.changeImg = function(e) {
-        console.log("Yay");
+    $scope.restart = function() {
+        $scope.step = 1;
     }
-    
 });
-// $('#fileField').change(function() {
-//     // var imageb64 = $(this).val();
-//     // imageb64=imageb64.substr(imageb64.indexOf(',')+1,imageb64.length);
-//     $('#showSrcField').val(this.files[0].name);
-// });
+$('#fileField').change(function() {
+    // var imageb64 = $(this).val();
+    // imageb64=imageb64.substr(imageb64.indexOf(',')+1,imageb64.length);
+    $('#showSrcField').val(this.files[0].name);
+});
