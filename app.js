83aaa16f131fee29edce33595defd7c8649e4f1d
sunboyy <sura_sun@hotmail.com>
2017-12-25 22:55:10 +0700

    Now the image can be cropped

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('node_modules/bootstrap/dist'));
 app.use('/js', express.static('node_modules/jquery/dist'));
-app.use('/js', express.static('node_modules/cropper/dist'));
+// app.use('/js', express.static('node_modules/cropper/dist'));
 app.use('/css', express.static('node_modules/font-awesome/css'));
 app.use('/fonts', express.static('node_modules/font-awesome/fonts'));
 app.use('/js/angular.min.js', express.static('node_modules/angular/angular.min.js'));
+app.use('/js/croppie.min.js', express.static('node_modules/croppie/croppie.min.js'));
+app.use('/css/croppie.css', express.static('node_modules/croppie/croppie.css'));
 app.use(express.static('public'));
 
 require('./router')(app);
--- public/js/cropnframe.js
+++ public/js/cropnframe.js
@@ -1,20 +1,78 @@
-var app = angular.module("cropnframe", []);
-app.controller('cropnframe', function($scope) {
-    $scope.step = 1;
-    $scope.next = function() {
-        $scope.step++;
+let page = 1;
+function changePage() {
+    $croppie.croppie('result', {
+        type: 'base64',
+        size: {
+            width: 300,
+            height: 300
+        }
+    }).then(function (resp) {
+        $('#croppedImg').attr('src', resp)
+    })
+
+    for (let i=1; i<=3; i++) {
+        $('#indicator-page-'+i).removeClass('steplabel-prev')
+        $('#indicator-page-'+i).removeClass('steplabel-now')
+        $('#indicator-page-'+i).removeClass('steplabel-next')
+        $('#indicator-page-'+i).addClass((page==i) ? 'steplabel-now' : ((page < i) ? 'steplabel-next' : 'steplabel-prev'))
+        
+        $('#page-'+i).hide()
     }
-    $scope.restart = function() {
-        $scope.step = 1;
+    $('#page-'+page).show()
+    if (page == 2) {
+        readFile($('#fileField')[0])
+    }
+    if (page == 3) {
+        $('#nextBtn').hide()
+        $('#restartBtn').show()
+    }
+    else {
+        $('#nextBtn').show()
+        $('#restartBtn').hide()
+    }
+}
+$('#nextBtn').click(function () {
+    page++;
+    changePage();
+});
+$('#restartBtn').click(function() {
+    page = 1;
+    changePage();
+});
+var $croppie = $('#cropping').croppie({
+    boundary: {
+        width: 300,
+        height: 300
+    },
+    viewport: {
+        width: 200,
+        height: 200
     }
 });
-$('#fileField').change(function() {
-    // var imageb64 = $(this).val();
-    // imageb64=imageb64.substr(imageb64.indexOf(',')+1,imageb64.length);
-    $('#showSrcField').val(this.files[0].name);
-    var fileReader = new FileReader();
-    fileReader.onload = function(e) {
-        console.log(e.target.result);
+changePage();
+function readFile(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $croppie.croppie('destroy');
+            $croppie.croppie({
+                boundary: {
+                    width: 300,
+                    height: 300
+                },
+                viewport: {
+                    width: 200,
+                    height: 200
+                },
+                url: e.target.result
+            });
+        }
+        reader.readAsDataURL(input.files[0]);
     }
-    fileReader.readAsDataURL(this.files[0].fillPath);
+    else {
+        alert("Sorry. Your browser doesn't support FileReader API");
+    }
+}
+$('#fileField').change(function () {
+    $('#showSrcField').val(this.files[0].name);
 });
